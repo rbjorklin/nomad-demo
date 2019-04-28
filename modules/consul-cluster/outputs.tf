6bc3c3b3eb1d0787e9ea8bbe340c6a8c5ee11bda
Robin Björklin <robin.bjorklin@gmail.com>
2019-04-27 18:34:05 -0700

    feat: add terraform outputs

--- modules/consul-cluster/outputs.tf
+++ modules/consul-cluster/outputs.tf
@@ -1,3 +1,23 @@
 output "ips" {
   value = ["${hcloud_server.nodes.*.ipv4_address}"]
 }
+
+output "status" {
+  value = ["${hcloud_server.nodes.*.status}"]
+}
+
+output "server_type" {
+  value = ["${hcloud_server.nodes.*.server_type}"]
+}
+
+output "location" {
+  value = ["${hcloud_server.nodes.*.location}"]
+}
+
+output "datacenter" {
+  value = ["${hcloud_server.nodes.*.datacenter}"]
+}
+
+output "image" {
+  value = ["${hcloud_server.nodes.*.image}"]
+}
